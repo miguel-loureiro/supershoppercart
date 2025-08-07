bec3e8dbeb0c1b3b24f52bf19cb7cf0a9478a316
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2025-08-07 13:14:13 +0100

    rename main class

--- backend/src/main/java/com/supershopcart/SuperShopperCartApplication.java
+++ backend/src/main/java/com/supershopcart/SuperShopperCartApplication.java
@@ -1,19 +1,14 @@
 package com.supershopcart;
 
-import com.supershopcart.seeder.ShopCartSeeder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Map;
-
 @SpringBootApplication
 @EnableScheduling
-public class SupershopcartApplication {
+public class SuperShopperCartApplication {
     public static void main(String[] args) {
         // A Spring Boot application will automatically use the active profile
         // set by environment variables (e.g., SPRING_PROFILES_ACTIVE) or
@@ -21,7 +16,7 @@ public class SupershopcartApplication {
         // We remove the hardcoded setting here to maintain flexibility.
 
         // Running the application
-        ConfigurableApplicationContext context = SpringApplication.run(SupershopcartApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SuperShopperCartApplication.class, args);
         Environment env = context.getEnvironment();
         String activeProfiles = String.join(", ", env.getActiveProfiles());
         System.out.println("Active Spring Profiles: [" + activeProfiles + "]");
