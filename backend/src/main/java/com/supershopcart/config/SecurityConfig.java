f20cf255afd351ede7a55776f5d40d052e4de205
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2025-08-09 12:53:25 +0100

    add /status permit all

--- backend/src/main/java/com/supershopcart/config/SecurityConfig.java
+++ backend/src/main/java/com/supershopcart/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/api/public/**",
                                 "/",
+                                "/status",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
